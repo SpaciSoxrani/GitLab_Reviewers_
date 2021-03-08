2ad75f67921638cd8ee1ffac34352d480307d258
SpaciSoxrani <thorloky007@gmail.com>
2021-03-08 13:30:07 +0500

    add parser in telegram bot

---
+++ src/main/java/TelegramBot/GR_BotApplication.java
@@ -0,0 +1,13 @@
+package TelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GR_BotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GR_BotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/TelegramBot/TelegramCommands/Command.java
@@ -0,0 +1,8 @@
+package TelegramBot.TelegramCommands;
+
+public enum Command {
+    NONE, NOTFORME,
+
+    NOTIFY,
+    START, HELP, ID
+}
